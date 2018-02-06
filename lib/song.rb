2d10cdc9a39d089382080b747c3568d66aa6f733
Eden Mesfun <emesfun@gmail.com>
2018-02-06 14:48:04 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -45,7 +45,7 @@ class Song
     @song=song
     song.gsub(/.mp3/, "") #deletes mp3
     song.gsub(/.[-].+/, "") #thundercat
-    song.gsub(/.+[-]./, "") #for love i come
+    song.gsub(/.mp3/, "").gsub(/.+[-]./, "") #for love i come
     binding.pry
     #song.gsub(/.mp3/, "").split(" - ")
     #song.new =
