f8f9eaae381efe471ea41b7b7b14561f4fea96ee
Eden Mesfun <emesfun@gmail.com>
2018-02-06 14:51:42 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -45,7 +45,6 @@ class Song
     @song=song
     song.gsub(/.[-].+/, "") #thundercat
     song.gsub(/.mp3/, "").gsub(/.+[-]./, "") #for love i come
-
   end #method
 
   def self.all #created by lab
