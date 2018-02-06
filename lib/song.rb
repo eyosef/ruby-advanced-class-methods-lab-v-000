dd7b9647477654597eced80458120b3456a4cca9
Eden Mesfun <emesfun@gmail.com>
2018-02-06 14:16:15 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -46,9 +46,8 @@ class Song
     #   song.name = info[0]
     #   song.artist_name = info[1]
     # end
-        info = song.gsub(/.mp3/, "").split(" - ") #removed .mps and parsed string based on '-'
-        info[0] = artist_name #FIX #"Thundercat"
-        info[1] = song.name #FIX #"For Love I Come"
+        song.gsub(/.mp3/, "").split(" - ") #removed .mps and parsed string based on '-'
+
         binding.pry
 
 
