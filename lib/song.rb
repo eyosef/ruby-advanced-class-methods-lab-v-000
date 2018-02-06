e9ebd7ed904b847869e1bb876b677c99a832c0ef
Eden Mesfun <emesfun@gmail.com>
2018-02-06 14:42:44 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -45,7 +45,7 @@ class Song
     @song=song
     song.name = song.gsub(/.[-].+/, "") #thundercat
     song.gsub(/.+[-]./, "") #for love i come
-
+    binding.pry
     #song.gsub(/.mp3/, "").split(" - ")
     #song.new =
   end #method
