8ae54f320d2e22c61185fbfc1079513f4d1b5d92
Eden Mesfun <emesfun@gmail.com>
2018-02-06 15:36:11 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -35,6 +35,7 @@ class Song
   end
 
   def self.new_from_filename(song) #working on
+    
     artist_name = song.gsub(/.[-].+/, "") #thundercat
     name = song.gsub(/.mp3/, "").gsub(/.+[-]./, "") #for love i come
   end #method
