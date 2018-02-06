3272f95ecdb60c6bf8c54d1135f441481d32577c
Eden Mesfun <emesfun@gmail.com>
2018-02-06 13:42:19 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -32,6 +32,7 @@ class Song
 
   def self.alphabetical #working on - look at  Advanced Class Methods,self.normalize_names
     alpha = self.all.sort_by { |a,b| a <=> b }
+    alpha
     binding.pry
   end
 
