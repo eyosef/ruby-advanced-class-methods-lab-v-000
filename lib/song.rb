7cf65b2d2dd42b2663b133fe53ea96e392a56f34
Eden Mesfun <emesfun@gmail.com>
2018-02-06 15:12:21 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -34,7 +34,7 @@ class Song
     self.all.sort_by { |song| song.name }
   end
 
-  def song.new_from_filename(song) #working on
+  def song.new_from_filename #working on
     song.gsub(/.[-].+/, "") #thundercat
     song.gsub(/.mp3/, "").gsub(/.+[-]./, "") #for love i come
   end #method
